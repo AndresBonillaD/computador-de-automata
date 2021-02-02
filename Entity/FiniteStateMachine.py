6857423defbbbd73d41e589596608e2d22433aae
Andres Bonilla <aabonillad@unal.edu.co>
2021-02-02 14:45:22 -0500

    establecido menu, establecido metodo main(), elimanada clase transition

--- Entity/FiniteStateMachine.py
+++ Entity/FiniteStateMachine.py
@@ -10,6 +10,8 @@ class FiniteStateMachine:
     initialState = State
     controlUnit = State
 
+    accepted = 0
+
     # constructor of the class fsm
     def __init__(self, alphabet, states, initialState):
         self.alphabet = alphabet
@@ -20,30 +22,37 @@ class FiniteStateMachine:
     # block to process strings
     # frontier case, empty word!!!!!!!!
     def computeWord(self, word):
+        remaningSymbol = False
+
         print('Computing string: "{0}" , Length: {1} ...'.format(word, len(word)))
         print('- - -')
-        if len(word) == 1:
-            # base case -
+        if len(word) == 0:
+            print("cadena vacia")
+        elif len(word) == 1:
+            #### BASE CASE
             print('base case')
             print('Control Unit: {0}.   Symbol to process: {1}'.format(self.controlUnit.tag, word[0]))
 
             # change control unit - last one to process the end symbol in the word
-            if word[0] not in self.controlUnit.transitions:
-                    print('NO transition found')
+            if str(word[0]) not in self.controlUnit.transitions:
+                print('NO transition found')
+                remaningSymbol = True
             else:
                 # change the CU to the only destinyState (dfa)
                 desStates = self.controlUnit.transitions[word[0]]
                 print('Transition Found: {0}:{1}>{2}'.format(self.controlUnit.tag, word[0], desStates[0]))
                 print('- - -')
+                # simulate CONSUME SYMBOL
                 self.controlUnit = self.controlUnitChange(desStates[0])
 
-            # check for the final state reached in the machine
-            if self.controlUnit.isAccepted:
+            # check for the final state reached in the machine and for remaining symbols in the string
+            # satisfaction conditions for Acceptance of the word
+            if self.controlUnit.isAccepted and not remaningSymbol:
                 print('ACCEPTED STRING! the state {0} is Accepted.'.format(self.controlUnit.tag))
             else:
                 print('REJECTED STRING state {0} is NOT Accepted'.format(self.controlUnit.tag))
         else:
-            # recursive case
+            #### RECURSIVE CASE
             # change CU to the destiny states in the transitions dict in the current CU
             print('recursive case')
             print('Control Unit: {0}.   Symbol to process: {1}'.format(self.controlUnit.tag, word[0]))
@@ -51,17 +60,15 @@ class FiniteStateMachine:
             # check the possibility of computing the first symbol of word
             if word[0] not in self.controlUnit.transitions:
                     print('NO transitions found for {0}:{1}'.format(self.controlUnit.tag, word[0]))
+                    print('REJECTED STRING')
             else:
                 # change the CU to the only destinyState (dfa)
                 desStates = self.controlUnit.transitions[word[0]]
                 print('Transition Found: {0}:{1}>{2}'.format(self.controlUnit.tag, word[0], desStates[0]))
                 print('- - -')
                 self.controlUnit = self.controlUnitChange(desStates[0])
-                # recursive calling
+                # recursive calling CONSUME SYMBOL
                 self.computeWord(word[1:])
-
-
-
 
     # method to print on console the info representing the automata
     def showData(self):
--- View/interface.py
+++ View/interface.py
@@ -1,29 +1,17 @@
 # way to navigate the app
-from Controller.AutomataFileReader import AutomataFileReader
-from Entity.FiniteStateMachine import FiniteStateMachine
-from Entity.State import State
-from Entity.Transition import Transition
+from Controller.core import core
 
-# hardcoded dfa L = { a*b+ }
-# instance transitions
-q0Transitions = {'a': ['q0'], 'b': ['q1']}
-q1Transitions = {'b': ['q1']}
-# instance states
-q0 = State('q0', False, q0Transitions)
-q1 = State('q1', True, q1Transitions)
 
-# alphabet set
-a = {'a', 'b'}
-# states set
-s = {q0, q1}
-# initial state q0
-# instance dfa object
-fda0 = FiniteStateMachine(a, s, q0)
+def main():
 
-# start
+    #### START
 
-# fda0.showData()
-fda0.computeWord('')
+    myAppCore = core()
+    myAppCore.menuLoop()
 
-# fileReader = AutomataFileReader()
-# fileReader.openFile()
+    #### end of main()
+
+
+if __name__ == "__main__":
+    main()
+
